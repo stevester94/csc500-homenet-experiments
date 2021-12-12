4cb88a54f7b9f793ff3ce8f74af7b88d67cd891f
Steven Mackey <stevester94@gmail.com>
2021-12-11 20:46:17 -0800

    dumb logging

--- ORACLE/ptn/experiment_homenet_testing/conductor.py
+++ ORACLE/ptn/experiment_homenet_testing/conductor.py
@@ -20,12 +20,6 @@ base_parameters["device"] = "cuda"
 base_parameters["max_cache_items"] = 4.5e6
 
 base_parameters["desired_serial_numbers"] = ALL_SERIAL_NUMBERS
-# base_parameters["desired_serial_numbers"] = [
-#     "3123D52",
-#     "3123D65",
-#     "3123D79",
-#     "3123D80",
-# ]
 base_parameters["source_domains"] = [38,]
 base_parameters["target_domains"] = [
     20,
@@ -44,20 +38,17 @@ base_parameters["window_stride"]=50
 base_parameters["window_length"]=256
 base_parameters["desired_runs"]=[1]
 base_parameters["num_examples_per_device"]=75000
-base_parameters["num_examples_per_device"]=7500
 
-base_parameters["n_shot"] = 3
+base_parameters["n_shot"] = 10
 base_parameters["n_way"]  = len(base_parameters["desired_serial_numbers"])
-base_parameters["n_query"]  = 2
-base_parameters["n_train_tasks"] = 2000
-base_parameters["n_train_tasks"] = 100
-base_parameters["n_val_tasks"]  = 100
-base_parameters["n_test_tasks"]  = 100
+base_parameters["n_query"]  = 10
+base_parameters["n_train_tasks"] = 1000
+base_parameters["n_val_tasks"]  = 1000
+base_parameters["n_test_tasks"]  = 2000
 
-base_parameters["n_epoch"] = 100
-base_parameters["n_epoch"] = 3
+base_parameters["n_epoch"] = 20
 
-base_parameters["x_net"] =     [# droupout, groups, 512 out
+base_parameters["x_net"] =     [
     {"class": "nnReshape", "kargs": {"shape":[-1, 1, 2, 128]}},
     {"class": "Conv2d", "kargs": { "in_channels":1, "out_channels":256, "kernel_size":(1,7), "bias":False, "padding":(0,3), },},
     {"class": "ReLU", "kargs": {"inplace": True}},
@@ -82,7 +73,6 @@ parameters = base_parameters
 # These will get permuted so we cover every possible case
 custom_parameters = {}
 custom_parameters["seed"] = [1337, 420]
-# custom_parameters["patience"] = [10, 11]
 
 
 import copy
